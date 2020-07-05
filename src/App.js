56e335dc3e13e273f16d06ee6d2470c5b9362923
Febryardi13 <febryardiwork@gmail.com>
2020-07-05 22:06:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HomePage from './components/homepage/homepage.component'
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
