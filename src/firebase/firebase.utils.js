b890a3cca0f4dfedfeaef3f503bdd10d10e49052
Febryardi13 <febryardiwork@gmail.com>
2020-07-13 23:28:10 +0700

    add cartDropDown component  with toogle

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,9 @@ const config = {
     appId: "1:153378941082:web:16f804df0a3b9598585723",
     measurementId: "G-1ZDDPYL96L"
   };
-  
+
+  firebase.initializeApp(config);
+
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
@@ -40,8 +42,8 @@ const config = {
     return userRef;
 
   }
-  firebase.initializeApp(config);
 
+  // export const auth = firebase.auth();
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
---
+++ src/redux/cart/cart.action.js
@@ -0,0 +1,5 @@
+import { CartActionTypes } from './cart.types'
+
+export const toogleHiddenCart = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
