3e705aa6e15913c5e228a81cc8947c4c56c9970e
Febryardi13 <febryardiwork@gmail.com>
2020-08-28 11:28:21 +0700

    remove enforce https

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 const compression = require('compression')
-const enforce = require('https')
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config()
 
@@ -15,12 +14,10 @@ const port = process.env.PORT || 5000
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 app.use(cors())
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(enforce.HTTPS({ trustProtoHeader: true }))
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get("*", function(req, res){
